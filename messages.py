1ced399d8511bfeb6e1823079cad649898a20061
Rodrigo Valla <rodrigovalla@protonmail.ch>
2021-11-18 20:11:11 -0300

    Adding challenges explanations...

--- messages.py
+++ messages.py
@@ -50,8 +50,10 @@ class Messages():
 		return m
 
 	#Building the message to send the solution from a challenge...
-	def build_solution_message(self, solution, type):
-		m = "<b>Solución:</b> " + solution + "\n\n"
+	def build_solution_message(self, solution, type, explanation):
+		m = "<b>Solución:</b> " + solution + "\n"
+		if not explanation == None:
+			m += explanation + "\n\n"
 		m += "¿Quérés seguir? Mandá /" + type
 		return m
 
@@ -70,16 +72,16 @@ class Messages():
 			"rompecabezas y magia. Lo hacemos para celebrar el ingenio de <b>Martin Gardner</b> y <b>Jaime Poniachik</b>. " + \
 			"Podés enterarte de todas las novedades del festival en <a href='https://www.instagram.com/festivaldelingenio/'>" + \
 			"Instagram</a>.\n\n" + \
-			"Mi contenido incluye algunos palíndromos y acertijos populares pero también contenido original de varios " + \
-			"autores: <b>Rodolfo Kurchan</b>, <b>Esteban Grinbank</b>.\n\n" + \
+			"Mi contenido incluye algunos palíndromos y acertijos populares pero también contenido original de " + \
+			"<b>Rodolfo Kurchan</b> y <b>Esteban Grinbank</b>.\n\n" + \
 			"Mi administrador es @rvalla, escribile si me encontrás algún problema."
 		return m
 
 	#The message triggered with /help command...
 	def build_help_message(self):
 		m = "Podés pedirme distintas cosas. Acá te dejo mis comandos más divertidos:\n\n" + \
-			"> Mandame /rebus o /acertijo para que te desafíe acá mismo\n" + \
-			"> Mandame /palindromo para que te sorprenda con una oración que se lee al derecho y al revés\n" + \
-			"> Mandame /reversible seguido de un número si querés ver un número escrito de manera reversible\n" + \
-			"> Mandame /video si querés que comparta con vos un video de algunas de las charlas del festival"
+			"> Mandame /rebus o /acertijo para que te desafíe acá mismo.\n" + \
+			"> Mandame /palindromo para que te sorprenda con una oración que se lee al derecho y al revés.\n" + \
+			"> Mandame /reversible seguido de un número si querés ver un número escrito de manera reversible.\n" + \
+			"> Mandame /video si querés que comparta con vos un video de algunas de las charlas del festival."
 		return m
